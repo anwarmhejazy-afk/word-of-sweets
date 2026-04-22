6d913dc314998c510446ff2452e7abe868e59b35
Anwar Hegazy <anwarmhejazy@gmail.com>
2026-04-22 15:47:32 +0400

    Fix Supabase config

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://roubmrtlctofykokqddv.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_fOaDO0rPk6PzzHudxi-WZQ_QlxhXrwE";
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
